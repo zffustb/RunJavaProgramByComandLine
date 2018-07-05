379308e22b175c885f21fcc8a82674ed3dce8934
zffustb <15600758699@163.com>
2018-07-05 14:14:31 +0800

    添加了打印输出第3行行文字的代码

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String args[]) throws Exception{
 		System.out.println("Hello JAVA world.");
 		System.out.println("The second line.");
+		System.out.println("The Third line.");
 	}
 }
